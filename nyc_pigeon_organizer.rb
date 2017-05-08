f6ad868389ad3f9690a619e425f486b578d26464
Sarah Sweat <sarahsweat@gmail.com>
2017-05-08 02:10:39 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
+require "pry"
+
+
 def nyc_pigeon_organizer(data)
   # write your code here!
-end
+  new_hash = {}
+
+   data.each do |trait, options|
+     # Hash Iteration
+     # trait = color/gender/lives
+     # options = multiple options for trait
+     
+     options.each do |option, pigeons|
+       # Hash Iteration
+       # option = specific trait option
+       # pigeons = array of pigeons
+       
+       pigeons.each do |pigeon|
+         # Array Iteration
+         new_hash[pigeon] ||= {}
+         new_hash[pigeon][trait] ||= []
+         new_hash[pigeon][trait] << option.to_s
+       end
+     end
+   end
+
+   new_hash
+ end
